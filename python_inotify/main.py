9ef34ce6a07c8cfd8e3d8e087d030d62218ff244
VM 82 <vm-82@vagrant.vm>
2024-09-11 12:39:08 +0000

    check second run

--- python_inotify/main.py
+++ python_inotify/main.py
@@ -12,8 +12,17 @@ def _main():
 
     i = inotify.adapters.InotifyTree(watched_dir)
 
+    print("~~~~~~~~~~~  first call")
     with open(filename, 'a') as file:
         file.write(datetime.now().isoformat())
+
+    events = i.event_gen(yield_nones=False, timeout_s=1)
+    events = list(events)
+
+    pprint(events)
+    print("~~~~~~~~~~~  second call")
+    with open(filename, 'a') as file:
+        file.write(datetime.now().isoformat()+"seconds call")
 
     events = i.event_gen(yield_nones=False, timeout_s=1)
     events = list(events)
