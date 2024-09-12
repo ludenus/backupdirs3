c8a697af0af46351eccb98f500e5c52069b9a919
Andrey Andreyev <x0153362@gmail.com>
2024-09-12 20:24:20 +0200

    add github actions

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 set -x
+
+if [ -z "$VERSION" ]; then
+    [ -z "`git tag -l`" ] && git tag 0.0.1
+    export VERSION="`git describe --tags`"
+fi
+    
 poetry run poetry export -f requirements.txt --output requirements.txt --without-hashes
 poetry run pip install pyinstaller
 poetry run pyinstaller --onefile python_inotify/main.py
+
+cp ./dist.main ./dist/inotify
