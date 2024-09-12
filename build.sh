3646fdedbd915ac8a848d0b925076732e8ae59b0
Andrey Andreyev <x0153362@gmail.com>
2024-09-12 21:24:27 +0200

    Refactor build.sh to use poetry run for commands

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ if [ -z "$VERSION" ]; then
     export VERSION="`git describe --tags`"
 fi
     
+poetry run poetry install
 poetry run poetry export -f requirements.txt --output requirements.txt --without-hashes
 poetry run pip install pyinstaller
 poetry run pyinstaller --onefile python_inotify/main.py
