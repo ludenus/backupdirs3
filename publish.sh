43a49bcabec9341cbc7721e3599c8f84f2f7abfe
Andrey Andreyev <x0153362@gmail.com>
2024-09-23 19:29:09 +0200

    workaround for github actions re-run

--- publish.sh
+++ publish.sh
@@ -2,6 +2,17 @@
 set -e
 set -x
 
-poetry config repositories.testpypi https://test.pypi.org/legacy/
-poetry config pypi-token.testpypi ${PYPI_TEST_TOKEN}
-poetry publish --repository testpypi
+PACKAGE_NAME=backupdirs3
+VERSION=$(poetry version -s)
+EXISTS=$(curl -s https://test.pypi.org/pypi/$PACKAGE_NAME/$VERSION/json || echo "not found")
+if [[ $EXISTS == "not found" ]]; then
+    echo "Version $VERSION does not exist, proceeding with upload."
+    poetry config repositories.testpypi https://test.pypi.org/legacy/
+    poetry config pypi-token.testpypi ${PYPI_TEST_TOKEN}
+    poetry publish --repository testpypi
+
+else
+    echo "Version $VERSION already exists, skipping upload."
+    exit 0
+fi
+
