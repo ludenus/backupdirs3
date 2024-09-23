1f88b99ebfefd40246f05cd5120a4dfdc372b332
Andrey Andreyev <x0153362@gmail.com>
2024-09-23 20:23:33 +0200

    add Publish to PyPI Main Repo

--- publish.sh
+++ publish.sh
@@ -2,15 +2,21 @@
 set -e
 set -x
 
+: ${PYPI_TOKEN:?"ERROR: PYPI_TOKEN must be specified!"}
+: ${PYPI_UPLOAD_URL:?"ERROR: PYPI_UPLOAD_URL must be specified!"}
+: ${PYPI_CHECK_URL:?"ERROR: PYPI_CHECK_URL must be specified!"}
+
+PYPI_REPO_NAME=$(echo "$PYPI_UPLOAD_URL" | awk -F[/:] '{print tolower($4)}' | tr -d '.')
 PACKAGE_NAME=backupdirs3
 VERSION=$(poetry version -s)
-EXISTS=$(curl -fs https://test.pypi.org/pypi/$PACKAGE_NAME/$VERSION/json || echo "not found")
+
+EXISTS=$(curl -fs ${PYPI_CHECK_URL}/${PACKAGE_NAME}/${VERSION}/json || echo "not found")
+
 if [[ $EXISTS == "not found" ]]; then
     echo "Version $VERSION does not exist, proceeding with upload."
-    poetry config repositories.testpypi https://test.pypi.org/legacy/
-    poetry config pypi-token.testpypi ${PYPI_TEST_TOKEN}
-    poetry publish --repository testpypi
-
+    poetry config repositories.${PYPI_REPO_NAME} ${PYPI_UPLOAD_URL}
+    poetry config pypi-token.${PYPI_REPO_NAME} ${PYPI_TOKEN}
+    poetry publish --repository ${PYPI_REPO_NAME}
 else
     echo "Version $VERSION already exists, skipping upload."
     exit 0
